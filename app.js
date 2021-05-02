c6d4d2e5930eca3f46c2de9a3600d21fe94a877d
yashimwong <yashimwong@gmail.com>
2021-05-02 21:19:11 +0800

    feat: use express instead of basic http. Added public folder to server static files

--- app.js
+++ app.js
@@ -1,41 +1,18 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express = require("express");
+const path = require("path");
 
-// get all files
-const home_page = readFileSync("./navbar-app/index.html");
-const styles = readFileSync("./navbar-app/styles.css");
-const browser_js = readFileSync("./navbar-app/browser-app.js");
-const logo_svg = readFileSync("./navbar-app/logo.svg");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(home_page);
-    res.end();
-  } else if (url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(styles);
-    res.end();
-  } else if (url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(browser_js);
-    res.end();
-  } else if (url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(logo_svg);
-    res.end();
-  } else if (url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>About Page</h1>");
-    res.end();
-  } else {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>Page not found</h1>");
-    res.end();
-  }
+app.use(express.static("./public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
-server.listen(5000, () => {
-  console.log("Server started on http://localhost:5000");
+app.all("*", (req, res) => {
+  res.status(400).send("Resource not found");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on http://localhost:5000");
 });
