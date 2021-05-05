9b67265a5e924f9c4a002477c5c8ff8b06912b0d
yashimwong <yashimwong@gmail.com>
2021-05-05 23:14:58 +0800

    api - added morgan logging middleware

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
+const morgan = require("morgan");
 const auth = require("./authorize");
 const logger = require("./logger");
 
 // Pass in middleware to app.use to use it on all route
 // order matters. Only those below this uses the middleware
-app.use([auth, logger]);
+app.use([morgan("tiny"), express.static("./public")]);
 
 app.get("/", (req, res) => {
   res.send("Home");
@@ -15,6 +16,11 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   console.log(req.user);
   res.send("About");
+});
+
+app.get("/api/items", (req, res) => {
+  console.log(req.user);
+  res.send("Items");
 });
 
 app.listen(5000, () => {
