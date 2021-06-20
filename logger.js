c2e73946a5175712bcaacd17196dfe82cd90c72f
yashimwong <yashimwong@gmail.com>
2021-06-20 10:29:23 +0800

    cleanup

--- logger.js
+++
@@ -1,8 +0,0 @@
-const logger = (req, res, next) => {
-  const { method, url } = req;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
-
-module.exports = logger;
