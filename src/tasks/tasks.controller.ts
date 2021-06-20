19f9991da71ad1b7025aa245e4c1434b54826251
yashimwong <yashimwong@gmail.com>
2021-06-20 15:11:43 +0800

    feat: add tasks endpoint

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,13 @@
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+}
