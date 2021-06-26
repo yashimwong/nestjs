b63fdf6b2e9ac2bc974700c0ece3e9771d61c5fd
yashimwong <yashimwong@gmail.com>
2021-06-26 09:05:47 +0800

    feat: add validation for getTask query

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsIn, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../task.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsIn([TaskStatus.OPEN, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search: string;
 }
