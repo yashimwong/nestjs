3bcf05f0f71b1b93bc98a49e58fc200ef7cc965f
yashimwong <yashimwong@gmail.com>
2021-07-01 09:15:13 +0800

    feat: add password salt and hash

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -5,20 +5,23 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.enity';
+import * as bcrypt from 'bcrypt';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
+    const salt = await bcrypt.genSalt();
+    const hashed_password = await bcrypt.hash(password, salt);
     const new_user = this.create({
       username,
-      password,
+      password: hashed_password,
     });
 
     try {
       await this.save(new_user);
     } catch (error) {
-      // duplicate username
+      // duplicate username error code
       if (error.code === '23505') {
         throw new ConflictException('Username already exists');
       } else {
