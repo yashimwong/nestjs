34779d3311e508bb7bccb99e27916da4fe46474a
yashimwong <yashimwong@gmail.com>
2021-08-22 00:41:38 +0800

    feat: add route guard

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksRepository } from './tasks.repository';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TasksRepository])],
+  imports: [TypeOrmModule.forFeature([TasksRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
